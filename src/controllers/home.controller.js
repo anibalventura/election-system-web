a5c9108bcf705794fa60b6fba0bba8b89fee5103
Anibal Ventura <anibalventura@icloud.com>
2022-08-11 19:25:52 -0400

    Update home and admin pages and re

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,4 +1,9 @@
 export const getIndex = (req, res) => {
+  if (req.session.isLoggedIn) {
+    req.flash("errors", "You are not authorized to access this section.");
+    return res.redirect("/admin");
+  }
+
   res.render("home/index", {
     pageTitle: "Home",
     homeActive: true,
