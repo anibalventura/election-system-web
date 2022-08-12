082ee42feb5348e9b909fa25d110a5aeab3c8c3d
Anibal Ventura <anibalventura@icloud.com>
2022-08-11 20:51:21 -0400

    Add citizen restrictions to start vote.

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,3 +1,6 @@
+import Citizen from "../models/citizen.model.js";
+import Election from "../models/election.model.js";
+
 export const getIndex = (req, res) => {
   res.render("home/index", {
     pageTitle: "Home",
@@ -6,8 +9,53 @@ export const getIndex = (req, res) => {
 };
 
 export const postVote = (req, res) => {
-  res.render("home/vote/index", {
-    pageTitle: "Vote",
-    voteActive: true,
-  });
+  const identificationDocument = req.body.identificationDocument;
+
+  Election.findAll()
+    .then((result) => {
+      const electionsResult = result.map((result) => result.dataValues);
+      let activeElection = false;
+
+      electionsResult.forEach((election) => {
+        if (election.status) {
+          activeElection = election.status;
+        }
+      });
+
+      if (activeElection) {
+        if (identificationDocument.length === 12) {
+          Citizen.findOne({
+            where: { identificationDocument: identificationDocument },
+          })
+            .then((citizen) => {
+              if (!citizen) {
+                req.flash("errors", "Citizen not found.");
+                return res.redirect("/");
+              }
+
+              if (citizen.status) {
+                return res.render("home/vote/index", {
+                  pageTitle: "Vote",
+                  voteActive: true,
+                });
+              } else {
+                req.flash("errors", "Citizen is inactive.");
+                return res.redirect("/");
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          req.flash("errors", "Invalid Citizen ID.");
+          return res.redirect("/");
+        }
+      } else {
+        req.flash("errors", "There is no active election.");
+        return res.redirect("/");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
